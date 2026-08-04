300763f656694c5391d500877e69614e5f218189
williamhoangbossman <williamhoangbase14@gmail.com>
2026-08-03 21:59:53 -0400

    Rafael + UI fixes

---
+++ scenes/CharacterScenes/rafael.gd
@@ -0,0 +1,11 @@
+extends Area2D
+
+## make sure to add detection script. 
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
--- scripts/MainScripts/ManagerScripts/GameManager.gd
+++ scripts/MainScripts/ManagerScripts/GameManager.gd
@@ -26,4 +26,4 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	print(currentDifficulty)
+	pass
